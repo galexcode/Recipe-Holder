c3870552655b60c73d792afbc11ac0db4dc0ce79
cody <CallahanRTS@gmail.com>
2013-04-15 16:30:42 -0700

    Using NIB files with IIDeckView for sliding menu system

---
+++ Recipe_Holder/MainViewController.m
@@ -0,0 +1,97 @@
+//
+//  MainViewController.m
+//  Recipe_Holder
+//
+//  Created by Cody Callahan on 4/15/13.
+//  Copyright (c) 2013 Callahan. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "IIViewDeckController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //use black bar style
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    self.navigationController.navigationBar.topItem.title = @"Recipes";
+    
+    //connect left bar button
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"left" style:UIBarButtonItemStyleBordered target:self.viewDeckController action:@selector(toggleLeftView)];
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return YES;
+}
+
+- (void)previewBounceLeftView {
+    [self.viewDeckController previewBounceView:IIViewDeckLeftSide];
+}
+
+- (void)previewBounceRightView {
+    [self.viewDeckController previewBounceView:IIViewDeckRightSide];
+}
+
+- (void)previewBounceTopView {
+    [self.viewDeckController previewBounceView:IIViewDeckTopSide];
+}
+
+- (void)previewBounceBottomView {
+    [self.viewDeckController previewBounceView:IIViewDeckBottomSide];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Recipe_Holder/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Recipe_Holder
+//
+//  Created by Cody Callahan on 4/15/13.
+//  Copyright (c) 2013 Callahan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
